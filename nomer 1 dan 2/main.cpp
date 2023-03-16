97f9f691c632a10655337641c9cf8cbf7c3d376e
nbilalifia <nabilaalifiarizky@gmail.com>
2023-03-16 09:58:50 +0700

    minggu 2

---
+++ nomer 1 dan 2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //nomer 1
+    //operasi penjumlahan dua bilangan
+    int a, b;
+    cin >> a;
+    cin >> b;
+    cout << "hasil penjumlahan : " << (a+b) << endl;
+
+    //nomer 2
+    //operasi pengurangan 2 bilangan
+    cin >> a;
+    cin >> b;
+    cout << "hasil pengurangan : "<< (a-b) << endl;
+
+    //total value elemen dalam array
+
+    return 0;
+}
---
+++ nomer 3 dan 4/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //nomer 3 dan 4
+    //penjumlahan dan rata-rata bilangan array
+  int input[100], jml_array, i, total, rata_rata;
+
+  cout << "Input jumlah element array: ";
+  cin >> jml_array;
+  cout << endl;
+
+  cout << "Input "<< jml_array << " angka (dipisah dengan enter): ";
+  cout << endl;
+
+  total = 0;
+  for(i = 0; i < jml_array; i++){
+    cout << "Angka ke-" << i+1 <<": ";
+    cin >> input[i];
+    total = total + input[i];
+    rata_rata = total/jml_array;
+  }
+
+  cout << endl;
+
+  cout << "Total penjumlahan dari "<< jml_array <<
+          " angka tersebut adalah: " << total << endl;
+
+  cout << "Jumlah Rata-rata bilangan tersebut adalah : "<< rata_rata << endl;
+
+  return 0;
+}
